5127ab27804270b1a0b5106cd7679babf60bc06b
Shirley Aguilar <saguilar@applaudostudios.dev>
2022-10-27 03:06:51 -0500

    start with install of ngrx and ngrx dev tools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { StoreModule } from "@ngrx/store";
-import { miReducer } from './app.reducer';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+
+import { StoreModule } from "@ngrx/store";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { miReducer } from './app.reducer';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -18,9 +21,16 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserAnimationsModule,
     HttpClientModule,
     AppRoutingModule,
+
+
     StoreModule.forRoot({
       mensaje: miReducer
-    })
+    }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 4
+    }),
+   // StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+   // StoreModule.forRoot({}, {})
   ],
   providers: [],
   bootstrap: [AppComponent]
